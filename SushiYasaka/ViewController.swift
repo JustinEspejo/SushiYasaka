64ae7571b166c1c4d836710063466484e10bbecf
Justin Espejo <justin_12_04@yahoo.com>
2016-02-25 14:33:36 -0500

    Initial Commit

---
+++ SushiYasaka/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SushiYasaka
+//
+//  Created by Justin Espejo on 2/25/16.
+//  Copyright © 2016 QC.quack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
