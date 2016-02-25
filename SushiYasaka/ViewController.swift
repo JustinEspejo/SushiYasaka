4c495dc3e3a704c409bde8c5a0ddb54d5cd1b358
Justin Espejo <justin_12_04@yahoo.com>
2016-02-25 15:12:28 -0500

    initial commit

--- SushiYasaka/ViewController.swift
+++ SushiYasaka/ViewController.swift
@@ -10,15 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    
+    
+    @IBOutlet weak var Open: UIBarButtonItem!
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        Open.target = self.revealViewController()
+        Open.action = Selector("revealToggle:")
+        
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
+    
+
 
 
 }
